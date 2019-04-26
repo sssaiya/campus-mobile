d90d4dd4ec003fccf50501e4c4f005522ee9a0cc
g9singh <g1singh@ucsd.edu>
2019-04-26 14:08:15 -0700

    set up listener for iOS and android

--- app/containers/locationContainer.js
+++ app/containers/locationContainer.js
@@ -1,21 +1,44 @@
 import React from 'react'
 import SystemSetting from 'react-native-system-setting'
+import GPSState from 'react-native-gps-state'
 import { connect } from 'react-redux'
 
+const locationIsOff = 1
 class LocationContainer extends React.Component {
 	async componentDidMount() {
-		const { updateLocationStatus, updateDiningSort } = this.props
-		SystemSetting.addLocationListener((enabled) => {
-			updateLocationStatus(enabled)
-			if (!enabled) {
-				updateDiningSort('A-Z')
+		const { updateLocationStatus } = this.props
+		GPSState.addListener((status) => {
+			switch (status) {
+				case locationIsOff:
+					updateLocationStatus(false)
+					break
+				case GPSState.NOT_DETERMINED:
+					console.log('Please, allow the location, for us to do amazing things for you!')
+					break
+
+				case GPSState.RESTRICTED:
+					GPSState.openLocationSettings()
+					break
+
+				case GPSState.DENIED:
+					console.log('It`s a shame that you do not allowed us to use location :(')
+					break
+
+				case GPSState.AUTHORIZED_ALWAYS:
+					// this is for android
+					updateLocationStatus(true)
+					break
+
+				case GPSState.AUTHORIZED_WHENINUSE:
+					// this is for ios
+					updateLocationStatus(true)
+					break
 			}
 		})
 	}
 
 	componentWillUnmount() {
-		this.onTokenRefreshListener()
-		this.messageListener()
+		GPSState.removeListener()
 	}
 
 	render() {
@@ -31,12 +54,6 @@ const mapDispatchToProps = (dispatch, ownProps) => (
 	{
 		updateLocationStatus: (enabled) => {
 			dispatch({ type: 'UPDATE_LOCATION_STATUS', enabled })
-			dispatch({ type: 'UPDATE_DINING' })
-			dispatch({ type: 'REORDER_DINING' })
-		},
-		updateDiningSort: (sortBy) => {
-			dispatch({ type: 'SET_DINING_SORT', sortBy })
-			dispatch({ type: 'REORDER_DINING' })
 		}
 	}
 )
