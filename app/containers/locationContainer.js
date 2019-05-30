847d21564010f09b723088420649c22dfcb9e445
Bryant, Charles <c3bryant@ucsd.edu>
2019-05-30 14:46:10 -0700

    fix a location permissions issue on initial launch for android

--- app/containers/locationContainer.js
+++ app/containers/locationContainer.js
@@ -2,39 +2,45 @@ import React from 'react'
 import GPSState from 'react-native-gps-state'
 import { connect } from 'react-redux'
 
-const locationIsOff = 1
 class LocationContainer extends React.Component {
 	async componentDidMount() {
 		const { updateLocationStatus } = this.props
+		// Get the current GPS state
+		GPSState.getStatus().then((status) => {
+			this.setLocationStatus(status, updateLocationStatus)
+		})
+		// Listen for changes to the GPS state
 		GPSState.addListener((status) => {
-			switch (status) {
-				case locationIsOff:
-					updateLocationStatus(false)
-					break
-				case GPSState.NOT_DETERMINED:
-					break
-
-				case GPSState.RESTRICTED:
-					updateLocationStatus(false)
-					break
-
-				case GPSState.DENIED:
-					updateLocationStatus(false)
-					break
-
-				case GPSState.AUTHORIZED_ALWAYS:
-					updateLocationStatus(true)
-					break
-
-				case GPSState.AUTHORIZED_WHENINUSE:
-					updateLocationStatus(true)
-					break
-			}
+			this.setLocationStatus(status, updateLocationStatus)
 		})
 	}
 
 	componentWillUnmount() {
 		GPSState.removeListener()
+	}
+
+	setLocationStatus = (status, updateLocationStatus) => {
+		const locationIsOff = 1
+
+		switch (status) {
+			case locationIsOff:
+				updateLocationStatus(false)
+				break
+			case GPSState.NOT_DETERMINED:
+				break
+			case GPSState.RESTRICTED:
+				updateLocationStatus(false)
+				break
+			case GPSState.DENIED:
+				updateLocationStatus(false)
+				break
+			case GPSState.AUTHORIZED_ALWAYS:
+				updateLocationStatus(true)
+				break
+			case GPSState.AUTHORIZED_WHENINUSE:
+				updateLocationStatus(true)
+				break
+		}
 	}
 
 	render() {
