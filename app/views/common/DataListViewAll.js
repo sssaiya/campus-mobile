d74c3af02fb719952a3ffa82b53e44f446f80729
vishant101 <vishant.singh@outlook.com>
2019-08-05 11:13:25 -0700

    Fix staring on view all events page

--- app/views/common/DataListViewAll.js
+++ app/views/common/DataListViewAll.js
@@ -2,21 +2,29 @@ import React from 'react'
 import DataListView from './DataListView'
 import logger from '../../util/logger'
 import css from '../../styles/css'
+import EventDataListView from '../events/EventDataListView';
 
 const DataListViewAll = ({ navigation }) => {
 	const { params } = navigation.state
 	const { title, data, item, card, onStarPress } = params
 	logger.ga('View Loaded: ' + title + ' (View All)')
-	return (
-		<DataListView
-			style={card ? css.DataList_card_list : css.DataList_full_list}
-			data={data}
-			scrollEnabled={true}
-			item={item}
-			card={card}
-			onStarPress={onStarPress}
-		/>
-	)
+	switch (item) {
+		case 'EventItem': {
+			return <EventDataListView />
+		}
+		default: {
+			return (
+				<DataListView
+					style={card ? css.DataList_card_list : css.DataList_full_list}
+					data={data}
+					scrollEnabled={true}
+					item={item}
+					card={card}
+					onStarPress={onStarPress}
+				/>
+			)
+		}
+	}
 }
 
 export default DataListViewAll
---
+++ app/views/events/EventDataListView.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import DataListView from '../common/DataListView'
+
+class EventDataListView extends Component {
+	_onStarPress = (eventId) => {
+		const { toggleEventStar } = this.props
+		toggleEventStar(eventId)
+	}
+
+	render() {
+		return (
+			<DataListView
+				data={this.props.eventsData}
+				scrollEnabled={false}
+				item="EventItem"
+				card={false}
+				onStarPress={this._onStarPress}
+			/>
+		)
+	}
+}
+
+const mapStateToProps = state => ({
+	eventsData: state.events.data
+})
+
+const mapDispatchToProps = dispatch => (
+	{
+		toggleEventStar: (eventId) => {
+			dispatch({ type: 'TOGGLE_STAR_ID', eventId })
+		}
+	}
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventDataListView)
